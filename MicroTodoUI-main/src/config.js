17bd190821f3c44990e9f4044b75ba94d6375771
panghalritu741-blip <panghalritu741@gmail.com>
2026-06-10 09:55:59 +0530

    Update config.js

--- MicroTodoUI-main/src/config.js
+++ MicroTodoUI-main/src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 
 const config = {
-    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://default-get-tasks-url',
-    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://default-delete-task-url',
-    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://default-create-task-url',
+    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://get-tasks.6weathers.com',
+    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://delete-task.6weathers.com',
+    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://add.6weathers.com',
   };
   
   export default config;
